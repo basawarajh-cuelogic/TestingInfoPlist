204116bc7b9ad67a10f653ad52cd44ab186cd039
Basawaraj <basawaraj.hiremath@cuelogic.co.in>
2017-04-27 16:53:54 +0530

    Initial Commit

---
+++ TestingInfoPlist/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestingInfoPlist
+//
+//  Created by Basawaraj on 27/04/17.
+//  Copyright © 2017 Cuelogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestingInfoPlist/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestingInfoPlist
+//
+//  Created by Basawaraj on 27/04/17.
+//  Copyright © 2017 Cuelogic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
